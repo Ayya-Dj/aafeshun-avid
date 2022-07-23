71116d402958a085f5d52ce1336c752b256907e7
Ayya <official.ayya@gmail.com>
2022-07-23 18:18:29 +0500

    Added daisyui and cart

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/daisyui.css') }}">
         <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 
         <!-- Scripts -->
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,9 @@
-<nav x-data="{ open: false }" class="af-text fixed top-1 right-1 lg:m-5 md:m-6 mx-8 my-6 w-screen">
-    <div @click="open = ! open" class="af-nav-button cursor-pointer z-10 absolute right-1" :class="{'open': open, 'closed': ! open}">
-        <div class="af-nav-burger"></div>
+<nav x-data="{ open: false, cart: false }" class="af-text fixed top-1 right-1 lg:m-5 md:m-6 mx-8 my-6 w-screen">
+    <div @click="open = ! open" class="af-nav-button cursor-pointer z-10 absolute left-1 ml-16" :class="{'open': open, 'closed': ! open}">
+        <div class="af-nav-burger md:hidden"></div>
+    </div>
+    <div @click="cart = ! cart" class="af-cart-button cursor-pointer z-10 absolute right-1" :class="{'open': cart, 'closed': ! cart}">
+        <div class="my-3 indicator"><span class="indicator-item indicator-top badge badge-secondary" id="cart-item-count">5</span><x-cart-icon :height=2 :width=2></x-cart-icon></div>
     </div>
     <div class="cursor-pointer z-10 fixed right-1 bottom-1 md:inline hidden">
         <div class="af-nav-social-media my-10"><x-sm-twitter-ol :height=2 :width=2></x-sm-twitter-ol></div>
@@ -10,25 +13,38 @@
     <div class="af-nav-bg w-screen min-h-screen absolute right-1 top-1 lg:-m-7 md:-m-8 -mx-10 -my-8 opacity-20" :class="{'open': open, 'closed': ! open}">
         <div class="max-w-7xl mx-auto lg:py-10 md:py-6 px-10 py-8 flex flex-wrap af-nav-inner">
 
-            <!-- Big nav -->
-            <div class="w-full md:flex flex-wrap hidden">
-                <div class="mx-auto flex items-center">
-                    <img src="{{asset('img/logo/logo.png')}}" class="w-auto md:h-20 h-12">
-                    <img src="{{asset('img/logo/text-white.png')}}" class="w-auto md:h-10 h-8 mx-5">
+            <!-- Smol nav for smol screens -->
+            <div class="w-full flex flex-wrap md:hidden">
+                <div class="w-full flex items-center">
+                    Nav
                 </div>
-                <div class="w-full">
-                    Big menu
+                <div>
+                    Links
                 </div>
             </div>
 
-            <!-- Smol nav for smol screens uwu -->
+        </div>
+    </div>
+    <div class="af-cart-bg w-screen min-h-screen absolute right-1 top-1 lg:-m-7 md:-m-8 -mx-10 -my-8 opacity-20" :class="{'open': cart, 'closed': ! cart}">
+        <div class="max-w-7xl mx-auto lg:py-10 md:py-6 px-10 py-8 flex flex-wrap af-cart-inner">
+
+            <!-- Big cart -->
+            <div class="w-full md:flex flex-wrap hidden">
+                <div class="mx-auto flex items-center">
+                    Shopping Cart
+                </div>
+                <div class="w-full">
+                    Big
+                </div>
+            </div>
+
+            <!-- Smol cart -->
             <div class="w-full flex flex-wrap md:hidden">
                 <div class="w-full flex items-center">
-                    <img src="{{asset('img/logo/logo.png')}}" class="w-auto md:h-20 h-12">
-                    <img src="{{asset('img/logo/text-white.png')}}" class="w-auto md:h-10 h-8 mx-5">
+                    Shopping Cart
                 </div>
                 <div>
-                    Smol menu
+                    Smol Items
                 </div>
             </div>
 
