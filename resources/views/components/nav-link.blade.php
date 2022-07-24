22ae1c25bbf4cf1462e3c4d8ee59a7e7680ade8f
Ayya <official.ayya@gmail.com>
2022-07-24 21:28:06 +0500

    Fixed nav links

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 text-md font-medium leading-5 af-link-active focus:outline-none transition duration-150 ease-in-out cursor-pointe'
+            ? 'inline-flex items-center px-4 pt-1 text-md font-medium leading-5 af-link-active focus:outline-none transition duration-150 ease-in-out cursor-pointe'
             : 'inline-flex items-center px-4 pt-1 border-b-2 border-transparent text-md font-medium leading-5 af-link-default focus:outline-none transition duration-150 ease-in-out cursor-pointer';
 @endphp
 
